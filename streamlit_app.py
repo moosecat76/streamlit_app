8085bbf68d67187a8480535239c40199092e97da
Sean Gardiner <seang@moosecat.info>
2023-11-19 15:38:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@
 
 import streamlit
 streamlit.title('My Parent\'s New Healthy Diner')
-
+    
 streamlit.header('Breakfast menu')
-streamlit.header('Omega 3 & Blueberry Oatmeal')
-streamlit.header('Kale, Spinich & Rocket Smoothie')
-streamlit.header('Hard-Boiled Free Range Egg')
+streamlit.header('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.header('🥗Kale, Spinich & Rocket Smoothie')
+streamlit.header('🐔Hard-Boiled Free Range Egg')
+streamlit.header('🥑🍞Avacardo Toast')
 
