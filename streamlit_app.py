ca16b4b92b34225a7e4950b41107c7f48f0882c2
Sean Gardiner <seang@moosecat.info>
2023-11-19 15:35:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@
 
 import streamlit
 streamlit.title('My Parent\'s New Healthy Diner')
+
+streamlit.header('Breakfast menu')
+streamlit.header('Omega 3 & Blueberry Oatmeal')
+streamlit.header('Kale, Spinich & Rocket Smoothie')
+streamlit.header('Hard-Boiled Free Range Egg')
+
