a47b4ac098063c0a684d26a7829e17ab3f4a638d
Sean Gardiner <seang@moosecat.info>
2023-11-19 15:41:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 #create the main python file
 
 import streamlit
-streamlit.title('My Parent\'s New Healthy Diner')
+streamlit.title('My Mum\'s New Healthy Diner')
     
-streamlit.header('Breakfast menu')
-streamlit.header('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.header('🥗Kale, Spinich & Rocket Smoothie')
-streamlit.header('🐔Hard-Boiled Free Range Egg')
-streamlit.header('🥑🍞Avacardo Toast')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinich & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free Range Egg')
+streamlit.text('🥑🍞Avacardo Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
